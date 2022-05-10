e4f655e3ba8a0d1053b3ba2aff8260e91c559761
BrytanniADJ <99222703+BrytanniADJ@users.noreply.github.com>
2022-05-09 21:11:47 -0300

    .

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./assests/css/reset.css">
+        <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="./assests/css/stilo.css">
+        <title>Home</title>
+    </head>
+    <body>
+        <header>
+        </header>
+            <main>
+                <div class="cabecalho">
+                    <ul class="botoes">
+                        <li class="click">
+                            <a class="button1" href="lista.php">Personalidades</a>
+                        </li>
+                        <li class="click">
+                            <a class="button2" href="detalhes.php">Detalhes</a>
+                        </li>
+                        <li class="click">
+                            <a class="button3" href="adicionar.php">Adicionar Irmã</a>
+                        </li>
+                    </ul>
+                </div>
+                  <section class="cabecalho2">
+                      <h2>Seja Bem vindo!</h2>
+                      <!-- nome, nivel, personalidade, habilidade-->
+                  </section>
+            </main>
+        <footer>
+        </footer>
+    </body>
+</html>
